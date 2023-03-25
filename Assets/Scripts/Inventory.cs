f908bd46884a1a6c5c874394a6ec97a69b528ec0
Simon Hetzer (MacBook) <simon.hetzer@icloud.com>
2023-03-25 11:10:34 +0100

    Shopping List UI

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -2,13 +2,33 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Inventory : MonoBehaviour
 {
     [SerializeField] private List<ItemObject> currentInventory;
 
     public Trader currentTrader = null;
+
+    [SerializeField] private Toggle[] shoppingListItems;
+
+    private void Start()
+    {
+        SetUpShoppingList();
+    }
+
+    private void SetUpShoppingList()
+    {
+        // TODO: Randomly pick some items that are needed
+        for (int i = 0; i < 4; i++)
+        {
+            var label = shoppingListItems[i].GetComponentInChildren<TextMeshProUGUI>();
+            var item = currentInventory[i];
+            label.SetText(item.name);
+        }
+    }
 
     public void Collect(string itemName)
     {
@@ -21,16 +41,28 @@ public class Inventory : MonoBehaviour
         {
             currentInventory.Add(new ItemObject(itemName));
         }
+
+        CheckShoppingList();
     }
 
     public bool CheckShoppingList()
     {
-        foreach (var item in currentInventory)
+        bool check = true;
+        for (int i = 0; i < currentInventory.Count; i++)
         {
+            var item = currentInventory[i];
             // Item is not required, skip check
             if (item.requiredAmount == 0) continue;
 
-            if (item.currentAmount < item.requiredAmount) return false;
+            if (item.currentAmount < item.requiredAmount)
+            {
+                shoppingListItems[i].SetIsOnWithoutNotify(false);
+                check = false;
+            }
+            else
+            {
+                shoppingListItems[i].SetIsOnWithoutNotify(true);
+            }
         }
         
         // Check passed for all required items
